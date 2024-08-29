cd457e08d0bbe6cb38883d6a4cfaa091c526c56a
matuchh7 <mvtuszektomek7@gmail.com>
2024-08-29 22:17:55 +0200

    country,pop,reg,cap and flag

---
+++ dom-utils.js
@@ -0,0 +1,56 @@
+const createInfoElement = (labelName, value) => {
+
+    const infoElement = document.createElement("div");
+    const labelElement = document.createElement("strong");
+    labelElement.innerText = labelName;
+    const valueElement = document.createElement("span");
+    valueElement.innerText = value;
+
+    infoElement.appendChild(labelElement);
+    infoElement.appendChild(valueElement);
+
+    return infoElement;
+}
+
+const createFlageImageElement = (country) => {
+    const imgContainerElement = document.createElement("div");
+    const imgElement = document.createElement("img");
+    imgElement.src = country.flagUrl;
+    imgElement.width = 160;
+    imgElement.height = 98;
+
+    
+imgContainerElement.appendChild(imgElement);
+
+    return imgContainerElement;
+}
+
+const createCountryItemElement = (country) => {
+    const countryElement = document.createElement("li");
+
+    const countryNameElement = document.createElement("span");
+    countryNameElement.innerText = country.name;
+
+    countryElement.appendChild(createFlageImageElement(country));
+
+    countryElement.appendChild(countryNameElement);
+
+    countryElement.appendChild(createInfoElement("Population: ", country.population));
+    countryElement.appendChild(createInfoElement("Region: ", country.region));
+    countryElement.appendChild(createInfoElement("Capital: ", country.capital));
+    return countryElement;
+}
+
+const createListElement = (countries) => {
+    const listElement = document.createElement("ul");
+    countries.forEach(country => {
+        listElement.appendChild(createCountryItemElement(country));
+    })
+    return listElement;
+}
+
+export const renderCountriesList = (countries) => {
+    const rootElement = document.querySelector("#root");
+    rootElement.appendChild(createListElement(countries));
+    console.log(countries);
+};
